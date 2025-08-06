7ebf415f1ec5dc791a0e83d56dfee86a3766e0d9
Alexander Peter <alexander.peter@bzz.ch>
2025-08-07 01:17:22 +0200

    #3 add e2e test

---
+++ frontend/e2e/poll-creation.test.ts
@@ -0,0 +1,19 @@
+import { Selector, ClientFunction } from 'testcafe';
+
+fixture`Umfrage E2E`.page`http://localhost:3000`;
+
+test('Neue Terminumfrage erstellen und Link kopieren', async (t) => {
+  const createPollTile = Selector('div').withText('Neue Terminumfrage erstellen');
+  await t.expect(createPollTile.exists).ok({ timeout: 10000 });
+  await t.click(createPollTile);
+  const pollTitleInput = Selector('input').withAttribute('name', 'pollTitle');
+  await t.expect(pollTitleInput.exists).ok({ timeout: 10000 });
+  await t.typeText(pollTitleInput, 'E2e-Meeting');
+  const createButton = Selector('button').withText('Erstellen');
+  await t.click(createButton);
+  const linkInput = Selector('input').withAttribute('value');
+  await t.expect(linkInput.exists).ok({ timeout: 10000 });
+  const pollLink = await linkInput.value;
+  console.log('Link:', pollLink);
+  await t.expect(pollLink).contains('/polls/', 'Poll-Link ist nicht korrekt');
+});
